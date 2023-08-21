9b5cc2bdf7ee24b88fb32df0a302b99b154bd40f
mrtnstkl <martin@stoeckl.me>
2023-08-21 23:48:43 +0200

    rework fuzzy endpoint

--- main.cpp
+++ main.cpp
@@ -19,6 +19,32 @@ void signal_handler(int signal)
 	}
 }
 
+template <typename T>
+std::string process_results(std::vector<fuzzy::result<T>> results, bool as_list = false)
+{
+	std::stringstream strstream;
+	if (!as_list)
+	{
+		assert(!results.empty());
+		strstream << results[0].element->meta;
+		return strstream.str();
+	}
+
+	if (results.empty())
+	{
+		return "[]";
+	}
+
+	strstream << "[\n";
+	for (size_t i = 0; i + 1 < results.size(); i++)
+	{
+		strstream << "\t" << results[i].element->meta << ",\n";
+	}
+	strstream << "\t" << results.back().element->meta << "\n]";
+	return strstream.str();
+}
+
+
 int main(int argc, char const *argv[])
 {
 	std::signal(SIGINT, signal_handler);
@@ -62,17 +88,35 @@ int main(int argc, char const *argv[])
 		<< "processed " << element_count << " entries" << std::endl;
 
 	server.Get(
-		"/:query",
+		"/fuzzy",
 		[&](const httplib::Request &req, httplib::Response &res)
 		{
-			auto query_string = req.path_params.at("query");
-			auto start = std::chrono::steady_clock::now();
-			auto query_result = database.fuzzy_search(query_string);
-			auto end = std::chrono::steady_clock::now();
 			using namespace std::chrono;
-			std::cout << "fuzzy-searched " << query_string << " in " << duration_cast<milliseconds>(end - start).count() << "ms: "
-					  << (query_result.empty() ? "not found" : query_result.best()[0].element->name) << std::endl;
-			res.set_content(query_result.empty() ? "{}" : query_result.best()[0].element->meta, "application/json");
+
+			if (!req.has_param("q"))
+			{
+				res.status = 400;
+				res.set_content("missing query parameter q", "text/plain");
+				return;
+			}
+
+			auto query_string = req.get_param_value("q");
+			bool respond_with_list = req.has_param("list") && req.get_param_value("list") == "yes";
+
+			auto start = steady_clock::now();
+			auto query_result = database.fuzzy_search(query_string);
+			auto end = steady_clock::now();
+			std::cout
+				<< "fuzzy-searched " << query_string << " in " << duration_cast<milliseconds>(end - start).count() << "ms: "
+				<< (query_result.empty() ? "not found" : query_result.best()[0].element->name) << std::endl;
+
+			if (!respond_with_list && query_result.empty())
+			{
+				res.status = 404;
+				res.set_content("no matches", "text/plain");
+				return;
+			}
+			res.set_content(process_results(query_result.best(), respond_with_list), "application/json");
 		});
 
 	std::cout << "\nstarting server on port " << port << std::endl;
